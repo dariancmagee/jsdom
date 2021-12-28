74c051ba3e4df505c3fd2b7d987a23b348e25ac6
dariancmagee <dariancmagee@gmail.com>
2021-12-28 18:01:02 -0500

    updated pages

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ let elementContainer = document.getElementById("container");
 let header1 = document.createElement("header");
 
 header1.textContent = "No Turning Back...";
-header1.style.fontSize = "x-large";
+header1.style.fontSize = "50px";
 header1.style.textAlign = "center";
 elementContainer.appendChild(header1);
-
 
 
 let button1 = document.createElement("button");
 
 button1.textContent = "Go Forward";
 button1.style.color = "blue";
+button1.style.fontSize = "20px";
 elementContainer.appendChild(button1);
 
 let button2 = document.createElement("button");
 
 button2.textContent = "Do Not Go Back";
 button2.style.color = "red";
+button2.style.fontSize = "20px";
 elementContainer.append(button2);
-
 
--- script2.js
+++ script2.js
@@ -6,11 +6,14 @@ let header1 = document.createElement("header");
 header1.textContent = "Your New Path";
 header1.style.fontSize = "x-large";
 header1.style.textAlign = "center";
+header1.style.fontSize = "55px";
 elementContainer.appendChild(header1);
 
 let body = document.createElement("p");
 
 body.textContent ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nesciunt impedit eligendi facilis exercitationem voluptas reprehenderit consequatur tempora quam eum? Quia quod quisquam laborum eligendi ea porro ipsam architecto quasi.";
+body.style.fontSize = "20px";
+body.style.fontWeight = "bold";
 elementContainer.appendChild(body);
 
 let list1 = document.createElement("ul");
@@ -18,10 +21,26 @@ let list2 = document.createElement("ul");
 let list3 = document.createElement("ul");
 
 list1.textContent = "What You Know";
+list1.style.fontSize = "25px";
+list1.style.color = "green";
 elementContainer.appendChild(list1);
 
 list2.textContent = "What Think You Know";
+list2.style.fontSize = "25px";
+list2.style.color = "green";
 elementContainer.appendChild(list2);
 
 list3.textContent = "What You Will Know";
+list3.style.fontSize = "25px";
+list3.style.color = "green";
 elementContainer.appendChild(list3);
+
+
+let header2 = document.createElement("header2");
+
+header2.textContent = "Choose Wisely...";
+header2.style.fontSize = "30px";
+header2.style.color = "red";
+header2.style.textAlign = "center";
+elementContainer.appendChild(header2);
+
